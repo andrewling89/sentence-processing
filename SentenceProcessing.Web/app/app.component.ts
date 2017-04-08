a159195af469c85e3e109ac62a1e3d31497ae8c5
Andrew Ling <andrew.ling89@gmail.com>
2017-04-08 15:16:51 +1000

    Added initial home page component.

---
+++ SentenceProcessing.Web/app/app.component.ts
@@ -0,0 +1,9 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: 'sentence-processing',
+    templateUrl: './app/app.component.html'
+})
+export class AppComponent {
+    constructor() { }
+}
---
+++ SentenceProcessing.Web/typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/bootstrap/index.d.ts" />
+/// <reference path="globals/core-js/index.d.ts" />
+/// <reference path="globals/grunt/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/systemjs/index.d.ts" />
+/// <reference path="globals/zone.js/index.d.ts" />
