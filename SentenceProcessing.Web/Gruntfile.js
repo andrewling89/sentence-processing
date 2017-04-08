8e0d9096281d111c08ebc7f2f4cc4c8dce699596
Andrew Ling <andrew.ling89@gmail.com>
2017-04-08 13:34:59 +1000

    Added Gruntfile.

---
+++ SentenceProcessing.Web/Gruntfile.js
@@ -0,0 +1,69 @@
+﻿module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-ts');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-open');
+
+    var rewrite = require("connect-modrewrite");
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        connect: {
+            options: {
+                middleware: function (connect, options, middlewares) {
+                    var rules = [
+                        "!\\.html|\\.js|\\.ts|\\.css|\\.svg|\\.jp(e?)g|\\.png|\\.gif|\\.woff|\\.woff2|\\.ttf$ /index.html"
+                    ];
+                    middlewares.unshift(rewrite(rules));
+                    return middlewares;
+                }
+            },
+            server: {
+                options: {
+                    port: 9000,
+                    base: './'
+                }
+            }
+        },
+        ts: {
+            build: {
+                src: ["**/*.ts", "!node_modules/**"],
+                options: {
+                    experimentalDecorators: true,
+                    module: 'commonjs',
+                    target: 'es5'
+                }
+            }
+        },
+        sass: {
+            build: {
+                options: {
+                    sourcemap: 'none'
+                },
+                files: {
+                    'css/style.css': 'css/style.scss'
+                }
+            }
+        },
+        watch: {
+            css: {
+                files: 'css/**/*.scss',
+                tasks: ['sass']
+            },
+
+            ts: {
+                files: '**/*.ts',
+                tasks: ['ts']
+            }
+        },
+        open: {
+            dev: {
+                path: 'http://localhost:9000/index.html'
+            }
+        }
+    });
+
+    grunt.registerTask('dev', ['connect', 'open', 'watch']);
+    grunt.registerTask('build', ['ts:build', 'sass:build']);
+}
